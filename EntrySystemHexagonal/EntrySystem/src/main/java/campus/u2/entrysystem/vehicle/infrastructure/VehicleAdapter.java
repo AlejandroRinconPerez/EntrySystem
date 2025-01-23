2c1d5df57f016a772b749b15e580491012ecfdc6
Alejandro_Rincon_Perez <168574435+AlejandroRinconPerez@users.noreply.github.com>
2025-01-23 17:28:11 -0500

    Update VehicleAdapter.java

--- EntrySystemHexagonal/EntrySystem/src/main/java/campus/u2/entrysystem/vehicle/infrastructure/VehicleAdapter.java
+++ EntrySystemHexagonal/EntrySystem/src/main/java/campus/u2/entrysystem/vehicle/infrastructure/VehicleAdapter.java
@@ -7,7 +7,9 @@ import jakarta.transaction.Transactional;
 import java.util.List;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class VehicleAdapter implements VehicleRepository {
 
     private final VehicleJpaRepository vehicleJpaRepository;
