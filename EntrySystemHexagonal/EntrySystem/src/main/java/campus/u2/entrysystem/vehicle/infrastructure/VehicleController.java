ab0835087775e7ca4e4c28c85d7339999b799712
Alejandro_Rincon_Perez <168574435+AlejandroRinconPerez@users.noreply.github.com>
2025-01-25 12:02:23 -0500

    vehicle controller

--- EntrySystemHexagonal/EntrySystem/src/main/java/campus/u2/entrysystem/vehicle/infrastructure/VehicleController.java
+++ EntrySystemHexagonal/EntrySystem/src/main/java/campus/u2/entrysystem/vehicle/infrastructure/VehicleController.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
 @RestController
 @RequestMapping("/api/vehicle")
@@ -18,30 +20,45 @@ public class VehicleController {
     public VehicleController(VehicleService vehicleService) {
         this.vehicleService = vehicleService;
     }
-
+//funciona
     @GetMapping
     public List<Vehicle> getAllVehicles() {
         return vehicleService.getAllVehicles();
     }
-
+//funciona
     @GetMapping("/{idVehicle}")
     public Optional<Vehicle> findbyId(@PathVariable Long idVehicle) {
         return vehicleService.findbyId(idVehicle);
     }
-
+//funciona
     @GetMapping("/plate/{plate}")
     public Optional<Vehicle> findByPlate(@PathVariable String plate) {
         return vehicleService.findVehicleByPlate(plate);
     }
 
     @PostMapping
-    public Vehicle saveVehicle(@RequestBody Vehicle vehicle) {
-        return vehicleService.saveVehicle(vehicle);
+    public ResponseEntity<Vehicle> createVehicle(@RequestBody Vehicle vehicle) {
+        return ResponseEntity.ok(vehicleService.saveVehicle(vehicle));
     }
-
-    @DeleteMapping("/{idVehicle}")
+// funciona
+    @DeleteMapping("{idVehicle}")
     public void deleteVehicle(@PathVariable Long idVehicle) {
         vehicleService.deleteVehicle(idVehicle);
     }
+    
+    
+  @PutMapping
+public ResponseEntity<Vehicle> updateVehicle(@RequestBody Vehicle vehicle) {
+    Optional<Vehicle> existingVehicleOpt = vehicleService.findbyId(vehicle.getIdVehicle());
+    if (existingVehicleOpt.isPresent()) {
+        Vehicle updatedVehicle = vehicleService.saveVehicle(vehicle);
+        return ResponseEntity.ok(updatedVehicle);
+    } else {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(null);
+    }
 }
 
+    
+    
+    
+}
